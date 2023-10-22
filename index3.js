c63367bf32569f22b93069e6f0094f6cb28c7059
DuMark Allman <dumark.allman@yahoo.com>
2023-10-22 12:56:11 -0400

    Update

--- index3.js
+++ index3.js
@@ -19,67 +19,70 @@ document.addEventListener("DOMContentLoaded", () => {
   
     createNewHero(newHero);
     heroForm.reset();
-});
-
-function createNewHero(hero) {
-  fetch("http://localhost:3000/heroes", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(hero),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(hero);
-      renderHeros();
-    });
-}
-
-function showPowers(hero) {
-  heroPowersContainer.replaceChildren();
-console.log("Card clicked!");
-  let powersDescription = document.createElement("p");
-  powersDescription.textContent = hero.powers;
-  console.log("Power desc:", powersDescription);
-  console.log(heroPowersContainer);
-  heroPowersContainer.appendChild(powersDescription);
-}
-
-function renderHeros() {
-  heroPowersContainer.replaceChildren();
-
-  fetch("http://localhost:3000/heroes", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(),
-  })
-    .then((response) => response.json())
-    .then((heroes) => {
-      const heroElements = heroes.map((hero) => {
-        const card = document.createElement("div");
-        const img = document.createElement("img");
-        img.src = hero.image;
-        const name = document.createElement("p");
-        name.textContent = hero.name;
-
-        card.appendChild(img);
-        card.appendChild(name);
-card.addEventListener("click", () => {
-          showPowers(hero);
-          console.log(hero);
-        });
-
-        heroContainer.append(card);
-        // return card;
-      });
-
-      //   heroContainer.append(...heroElements);
+  });
+  
+  function createNewHero(hero) {
+    fetch("http://localhost:3000/heroes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(hero),
     })
-    .catch((error) => {
-      console.error("Hello from the othersiiiiiiide!!!", error.message);
-      //added this to let me know if something is wrong with my code in this area
-    });
-}
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(hero);
+        renderHeros();
+      });
+  }
+  
+  function showPowers(hero) {
+    heroPowersContainer.replaceChildren();
+    console.log("Card clicked!");
+    let powersDescription = document.createElement("p");
+    powersDescription.textContent = hero.powers;
+    console.log("Power desc:", powersDescription);
+    console.log(heroPowersContainer);
+    heroPowersContainer.appendChild(powersDescription);
+  }
+  
+  function renderHeros() {
+    heroContainer.replaceChildren();
+  
+    fetch("http://localhost:3000/heroes", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(),
+    })
+      .then((response) => response.json())
+      .then((heroes) => {
+        const heroElements = heroes.map((hero) => {
+          const card = document.createElement("div");
+          card.classList = "hero-card";
+          const img = document.createElement("img");
+          img.src = hero.image;
+          const name = document.createElement("p");
+          name.textContent = hero.name;
+  
+          card.appendChild(img);
+          card.appendChild(name);
+  
+          card.addEventListener("click", () => {
+            showPowers(hero);
+            console.log(hero);
+          });
+  
+          heroContainer.append(card);
+          // return card;
+        });
+  
+        //   heroContainer.append(...heroElements);
+      })
+      .catch((error) => {
+        console.error("Hello from the othersiiiiiiide!!!", error.message);
+        //added this to let me know if something is wrong with my code in this area
+      });
+  }
+  
